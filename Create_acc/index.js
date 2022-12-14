3983028ddd2a0ba83caad447aeb5453cfe04ed1b
vbthang <bathangvu@gmail.com>
2022-12-14 22:14:28 +0700

    fix cfpassword

--- Create_acc/index.js
+++ Create_acc/index.js
@@ -2,6 +2,7 @@ function signup(e){
     event.preventDefault();
     var name = document.getElementById("name").value;
     var password = document.getElementById("password").value;
+    var cfpassword = document.getElementById("cfpassword").value;
     var Email = document.getElementById("Email").value;
     var sdt = document.getElementById("sdt").value;
     var user = {
@@ -12,6 +13,9 @@ function signup(e){
     };
     if(name == "" || password == "" || cfpassword == "" || Email == "" || sdt == ""){
         alert("vui lòng nhập đủ thông tin");
+    }
+    else if (password != cfpassword) {
+        alert("Sai cfps")
     }
     else{
         var json = JSON.stringify(user);
